29740adf04de45a5711ae0dd72faf9a3c8f0857c
rleoprleop rleop <rleop@naver.com>
2025-06-11 03:01:45 +0000

    transaction

---
+++ product/src/main/java/choreography/with/idempotency/domain/TransactionRepository.java
@@ -0,0 +1,7 @@
+package choreography.with.idempotency.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+
+}
